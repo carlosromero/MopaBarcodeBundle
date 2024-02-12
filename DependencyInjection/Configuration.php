143462ddd6633d4d02d2344e46daa21fbc165b4f
Carlos Romero <krlvisual@gmail.com>
2024-02-12 13:40:19 +0100

    Fix A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mopa_barcode');
+        if (method_exists(TreeBuilder::class, 'getRootNode')) {
+            $treeBuilder = new TreeBuilder('mopa_barcode');
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('mopa_barcode');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
